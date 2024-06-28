345457f446fd3049cc40c43c463d41e9b31f2249
kangeunhwa <dmdgi37@naver.com>
2024-06-29 01:27:59 +0900

    project-first-setting

---
+++ src/app/routes/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <p>LoginPage</p>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/utils/dummy.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dummy = () => {
+  return (
+    <div>
+      <h1>Dummy</h1>
+      <p>Dummy</p>
+    </div>
+  );
+};
+
+export default Dummy;
