6ddb379d851f328e432e0dc231ac9ca383d21fd4
kangeunhwa <dmdgi37@naver.com>
2024-07-11 08:27:48 +0900

    git-PR

--- src/app/(Auth)/sign-in/page.tsx
+++ src/app/(Auth)/sign-in/page.tsx
@@ -1,12 +1,12 @@
-'use client';
-import Button from '@/components/common/button';
-import Input from '@/components/common/input';
-import SocialLogin from '@/components/auth/social-login';
-import React from 'react';
-import { useFormState } from 'react-dom';
-import { login } from './action';
-import { PASSWORD_MIN_LENGTH } from '@/app/lib/constants';
-import Link from 'next/link';
+"use client";
+import Button from "@/components/common/button";
+import Input from "@/components/common/input";
+import SocialLogin from "@/components/auth/social-login";
+import React from "react";
+import { useFormState } from "react-dom";
+import { login } from "./action";
+import { PASSWORD_MIN_LENGTH } from "@/app/lib/constants";
+import Link from "next/link";
 
 export default function SignIn() {
   const [state, dispatch] = useFormState(login, null);
@@ -15,8 +15,7 @@ export default function SignIn() {
       <h1 className="font-bold text-lg pb-4">로그인</h1>
       <form
         action={dispatch}
-        className="w-full flex flex-col justify-center items-center gap-2 pb-4"
-      >
+        className="w-full flex flex-col justify-center items-center gap-2 pb-4">
         <Input
           name="email"
           type="email"
@@ -38,7 +37,7 @@ export default function SignIn() {
         <Button text="로그인" />
       </form>
       <div className="flex gap-2 my-4">
-        <span>계정이 없어요!</span>
+        <span>계정이 없어요! </span>
         <Link href="/sign-up" className="hover:underline underline-offset-4">
           회원가입
         </Link>
