641c14bd8b73feb011e2f7550011b409c49e948f
moko <ljun925@naver.com>
2024-07-12 15:42:30 +0900

    #21-fix-headerbar 링크 수정, 중복 파일 제거(소셜 로그인 컴포넌트)

--- src/components/common/header-bar.tsx
+++ src/components/common/header-bar.tsx
@@ -1,7 +1,7 @@
-"use client";
-import { User } from "@prisma/client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+'use client';
+import { User } from '@prisma/client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface HeaderBarProps {
   exists?: boolean;
@@ -11,17 +11,17 @@ export default function HeaderBar({ exists }: HeaderBarProps) {
   const pathname = usePathname();
   return (
     <div className="h-10 bg-main flex justify-between w-screen px-10 items-center">
-      <Link href={"/"}>logo</Link>
+      <Link href={'/'}>logo</Link>
       <div className="flex gap-6">
-        <Link href={"/asd"}>이력서</Link>
-        <Link href={"/#"}>
-          {pathname === "" ? <span>채용공고</span> : <span>채용공고</span>}
+        <Link href={'/recruit'}>이력서</Link>
+        <Link href={'/recruit'}>
+          {pathname === '' ? <span>채용공고</span> : <span>채용공고</span>}
         </Link>
-        <Link href={"/#"}>기술정보</Link>
-        <Link href={"/#"}>학습하기</Link>
+        <Link href={'/blog'}>기술정보</Link>
+        <Link href={'/quiz'}>학습하기</Link>
       </div>
-      <Link href={!exists ? "/sign-up" : "/profile"}>
-        {!exists ? "회원가입" : "마이프로필"}
+      <Link href={!exists ? '/sign-up' : '/profile'}>
+        {!exists ? '회원가입' : '마이프로필'}
       </Link>
     </div>
   );
