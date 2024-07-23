4eb088dd8fe6c15df41ba68505b1ad31e052b905
moko <ljun925@naver.com>
2024-07-23 18:31:01 +0900

    #5-feat-loginModal 스크린 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,27 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundColor: {
-        main: 'rgb(6, 182, 212)',
-      },
-      textColor: {
-        main: 'rgb(6, 182, 212)',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundColor: {
+                main: 'rgb(6, 182, 212)',
+            },
+            textColor: {
+                main: 'rgb(6, 182, 212)',
+                logo: 'rgb(5, 0, 232)',
+                headerText: 'rgb(53, 51, 53)',
+            },
+            fontSize: {
+                logoSize: '28px',
+                headerSize: '16px',
+            },
+        },
     },
-  },
-  plugins: [require('@tailwindcss/forms')],
+    plugins: [require('@tailwindcss/forms')],
 };
 export default config;
