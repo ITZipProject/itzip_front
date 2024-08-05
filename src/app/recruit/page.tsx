26449b8e8f6bba5f8ae55456d19f8ab922626157
= <younseu2@gmail.com>
2024-08-05 15:13:26 +0900

    style: 좌우폭 조정

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -86,7 +86,7 @@ const RecruitPage: React.FC = () => {
   };
 
   return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="mx-auto max-w-7xl overflow -x-hidden px-4">
       <h1 className="text-center text-2xl font-bold mb-6">채용 정보 목록</h1>
       <div className="flex space-x-4">
         <div className="w-1/4">
