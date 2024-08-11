cd9047a8e14caaf024c382ccd6224bceaa57d00f
moko <ljun925@naver.com>
2024-08-11 19:27:30 +0900

    #5 - button name 수정

--- src/app/(Auth)/sign-up/page.tsx
+++ src/app/(Auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Button from '@/components/common/button';
+import Button from '@/components/auth/authButton';
 import Input from '@/components/common/input';
 import React from 'react';
 import { useFormState } from 'react-dom';
