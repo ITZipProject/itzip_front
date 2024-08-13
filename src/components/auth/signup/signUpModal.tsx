441abe090fc46223a0df70e9b3ea9a199e6f420d
moko <ljun925@naver.com>
2024-08-14 05:42:58 +0900

    #57-modal-회원가입 모달 구현

--- src/components/auth/signup/signUpModal.tsx
+++ src/components/auth/signup/signUpModal.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import googleIcon from '../../../../public/Google.png';
 import githubIcon from '../../../../public/github.png';
 import { useModal } from '@/lib/context/ModalContext';
-import Modal from '../authModal';
+import AuthModal from '../authModal';
 import { Margin } from '@/components/common/margin';
 
 interface SignUpModalProps {
@@ -17,10 +17,14 @@ const SignUpModal: React.FC<SignUpModalProps> = ({ modalId }) => {
   if (!openModals.includes(modalId)) return null;
 
   return (
-    <Modal isOpen={true} onClose={() => closeModal(modalId)} title="회원가입이 필요해요!">
+    <AuthModal isOpen={true} onClose={() => closeModal(modalId)}>
+      <div>
+        <h2 className="text-[#6C6C6C] font-[500] text-[16px]">ITZIP을 다채롭게 즐기기 위해서는</h2>
+        <h2 className="text-[#171717] font-[700] text-[24px]">로그인이 필요해요!</h2>
+      </div>
       <div className="*:flex *:justify-center space-y-[16px]">
         <Margin height={'48px'} />
-        <button onClick={() => openModal('signinModal')} className="primary-btn">
+        <button onClick={() => openModal('signUpEmailModal')} className="primary-btn">
           <span>이메일로 회원가입하기</span>
         </button>
         {/* <LargeButton modalId="signinModal" btnType="primary-btn" text="이메일로 로그인" /> */}
@@ -48,7 +52,7 @@ const SignUpModal: React.FC<SignUpModalProps> = ({ modalId }) => {
           <h1 className="font-[400] text-[#0500E8] text-[13px] ">이미 회원이신가요?</h1>
         </button>
       </div>
-    </Modal>
+    </AuthModal>
   );
 };
 
