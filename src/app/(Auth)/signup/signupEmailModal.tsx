f555a5e26f10b0ec6cdb0c5b28dd614b09b7066c
moko <ljun925@naver.com>
2024-08-29 18:28:34 +0900

    #57-feat-이메일 인증 구현

--- src/app/(Auth)/signup/signupEmailModal.tsx
+++ src/app/(Auth)/signup/signupEmailModal.tsx
@@ -1,73 +1,150 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
 import Modal from '../auth/authModal';
 import Input from '../../../components/common/input';
 import Button from '../auth/authButton';
 import { useModal } from '@/lib/context/ModalContext';
 import { ChevronLeftIcon } from '@heroicons/react/16/solid';
 import { Margin } from '@/components/common/margin';
+import axios from 'axios';
+import { redirect } from 'next/navigation';
 
 interface SignInModalProps {
   modalId: string;
 }
 
-interface FormValues {
-  email: string;
-  password: string;
-  confirmPassword: string;
-  private: boolean;
-  service: boolean;
-}
-
 const SignUpEmailModal: React.FC<SignInModalProps> = ({ modalId }) => {
   const { openModals, closeModal, openModal } = useModal();
-  const {
-    control,
-    handleSubmit,
-    setValue,
-    watch,
-    formState: { errors },
-  } = useForm<FormValues>({
-    defaultValues: {
-      email: '',
-      password: '',
-      confirmPassword: '',
-      private: false,
-      service: false,
-    },
-  });
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [code, setCode] = useState('');
+  const [ErrorText, setErrorText] = useState('');
   const [allChecked, setAllChecked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOk, setIsOk] = useState(false);
+  const [isChecked, setIsChecked] = useState({
+    private: false,
+    service: false,
+  });
 
   // 모달이 열려 있는 경우에만 렌더링
   if (!openModals.includes(modalId)) return null;
 
-  const onSubmit = (data: FormValues) => {
-    console.log(data);
-    
-    // TODO: 회원가입 로직 추가
-    closeModal(modalId);
+  const handleReset = (field: 'email' | 'password' | 'confirmPassword' | 'code') => {
+    if (field === 'email') {
+      setEmail('');
+    } else if (field === 'password') {
+      setPassword('');
+    } else if (field === 'confirmPassword') {
+      setConfirmPassword('');
+    } else if (field === 'code') {
+      setCode('');
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    } else if (name === 'confirmPassword') {
+      setConfirmPassword(value);
+    } else if (name === 'code') {
+      setCode(value);
+    }
   };
 
   const handleAllCheckedChange = () => {
     const newCheckedStatus = !allChecked;
     setAllChecked(newCheckedStatus);
-    setValue('private', newCheckedStatus);
-    setValue('service', newCheckedStatus);
+    setIsChecked({
+      service: newCheckedStatus,
+      private: newCheckedStatus,
+    });
   };
 
   const handleAgreeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, checked } = e.target;
-    setValue(name as keyof FormValues, checked);
+    setIsChecked((prev) => ({
+      ...prev,
+      [name]: checked,
+    }));
     if (!checked) {
       setAllChecked(false);
     } else {
-      setAllChecked(['private', 'service'].every((key) => watch(key as any)));
+      setAllChecked(
+        Object.values({
+          ...isChecked,
+          [name]: checked,
+        }).every(Boolean),
+      );
     }
   };
 
+  const handleEmailCheck = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/checkDuplicateEmail`);
+      console.log(res.data);
+    } catch (err: any) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const handleVerifyCode = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/authEmail`, {
+        email,
+      });
+      console.log(res.data);
+      if (res.status === 200) {
+        setIsOk(true);
+        alert('인증 코드가 이메일로 전송되었습니다.');
+        console.log('인증 코드가 이메일로 전송되었습니다.');
+      }
+      if (res.status === 400) {
+        setIsOk(false);
+        alert('요청값이 올바르지 않습니다.');
+        console.log('요청값이 올바르지 않습니다.');
+      }
+    } catch (err: any) {
+      setErrorText('인증 코드 전송에 실패했습니다. 다시 시도해주세요.');
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const signUp = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/join`, {
+        email,
+        password,
+        password_check: confirmPassword,
+        auth_code: code,
+      });
+      if (res.status === 201) {
+        setIsOk(true);
+        console.log('리소스가 성공적으로 생성되었습니다.');
+      }
+      if (res.status === 400) {
+        setIsOk(false);
+        console.log('요청값이 올바르지 않습니다.');
+      }
+    } catch (err: any) {
+      setErrorText('다시 시도해주세요.');
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+      redirect('/profile');
+    }
+  };
   return (
     <Modal isOpen={true} onClose={() => closeModal(modalId)}>
       <button onClick={() => openModal('signUpModal')} className="flex items-center">
@@ -75,80 +152,55 @@ const SignUpEmailModal: React.FC<SignInModalProps> = ({ modalId }) => {
         <h1 className="font-[700] text-[24px]">이메일로 회원가입하기</h1>
       </button>
       <Margin height={'48px'} />
-      <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-4">
+      <form className="w-full space-y-4">
         <div className="flex items-center">
           <label htmlFor="email">이메일</label>
           <span className="text-[#E46969] ml-[2px]">*</span>
         </div>
-        <Controller
+        <Input
+          id="email"
           name="email"
-          control={control}
-          rules={{
-            required: '이메일은 필수입니다.',
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
-              message: '유효한 이메일 주소를 입력하세요.',
-            },
-          }}
-          render={({ field }) => (
-            <Input
-              id="email"
-              type="email"
-              placeholder="ID@example.com"
-              {...field}
-              onClick={() => setValue('email', '')}
-              errors={errors.email?.message}
-            />
-          )}
+          type="email"
+          value={email}
+          onChange={handleInputChange}
+          placeholder="ID@example.com"
+          required
+          minLength={2}
+          onClick={() => handleReset('email')}
         />
+        <Input name="code" type="text" placeholder="인증코드를 입력해주세요." errors={ErrorText} />
+
         <Button text="이메일 인증하기" modalId="" />
 
         <div className="flex items-center">
           <label htmlFor="password">비밀번호</label>
           <span className="text-[#E46969] ml-[2px]">*</span>
         </div>
-        <Controller
+        <Input
+          id="password"
           name="password"
-          control={control}
-          rules={{
-            required: '비밀번호는 필수입니다.',
-            minLength: {
-              value: 6,
-              message: '비밀번호는 최소 6자 이상이어야 합니다.',
-            },
-          }}
-          render={({ field }) => (
-            <Input
-              id="password"
-              type="password"
-              placeholder="비밀번호를 입력해주세요."
-              {...field}
-              onClick={() => setValue('password', '')}
-              errors={errors.password?.message}
-            />
-          )}
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          value={password}
+          onChange={handleInputChange}
+          required
+          minLength={2}
+          onClick={() => handleReset('password')}
         />
         <div className="flex items-center">
           <label htmlFor="confirmPassword">비밀번호 확인</label>
           <span className="text-[#E46969] ml-[2px]">*</span>
         </div>
-        <Controller
+        <Input
+          id="confirmPassword"
           name="confirmPassword"
-          control={control}
-          rules={{
-            required: '비밀번호 확인은 필수입니다.',
-            validate: (value) => value === watch('password') || '비밀번호가 일치하지 않습니다.',
-          }}
-          render={({ field }) => (
-            <Input
-              id="confirmPassword"
-              type="password"
-              placeholder="비밀번호를 입력해주세요."
-              {...field}
-              onClick={() => setValue('confirmPassword', '')}
-              errors={errors.confirmPassword?.message}
-            />
-          )}
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          value={confirmPassword}
+          onChange={handleInputChange}
+          required
+          minLength={2}
+          onClick={() => handleReset('confirmPassword')}
         />
         {/* 약관 동의 체크박스 */}
         <div className="*:p-[12px]">
@@ -169,7 +221,7 @@ const SignUpEmailModal: React.FC<SignInModalProps> = ({ modalId }) => {
               id="service"
               type="checkbox"
               name="service"
-              checked={watch('service')}
+              checked={isChecked.service}
               onChange={handleAgreeChange}
               required
               className="size-[16px] border border-[##C6C6C6] outline-none ring-0"
@@ -185,7 +237,7 @@ const SignUpEmailModal: React.FC<SignInModalProps> = ({ modalId }) => {
               id="private"
               type="checkbox"
               name="private"
-              checked={watch('private')}
+              checked={isChecked.private}
               onChange={handleAgreeChange}
               required
               className="size-[16px] border border-[##C6C6C6] outline-none ring-0 "
@@ -199,6 +251,9 @@ const SignUpEmailModal: React.FC<SignInModalProps> = ({ modalId }) => {
         </div>
 
         {/* 가입하기 버튼 */}
+        <span onClick={signUp}>
+          <Button text="가입하기" modalId="" />
+        </span>
         <Button text="가입하기" modalId="" />
 
         <div className="flex flex-col items-center">
@@ -211,6 +266,8 @@ const SignUpEmailModal: React.FC<SignInModalProps> = ({ modalId }) => {
           </button>
         </div>
       </form>
+      {isLoading ? 'Loading..' : <button onClick={handleEmailCheck}>이메일 중복확인</button>}
+      {isLoading ? 'Loading..' : <button onClick={handleVerifyCode}>이메일 인증하기</button>}
     </Modal>
   );
 };
