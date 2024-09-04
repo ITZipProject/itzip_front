f5ffe47f47a338941cc3b5ff4002127b3df5a61d
= <younseu2@gmail.com>
2024-09-04 15:36:23 +0900

    style: 카드 스타일 수정

--- src/components/recruit/joblist.tsx
+++ src/components/recruit/joblist.tsx
@@ -43,7 +43,6 @@ const JobList: React.FC<JobListProps> = ({ filteredJobs, handleBookmark }) => {
         </button>
       </div>
       <hr className="my-6 border-blue-700"/>
-      {/* <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6"> */}
       <div className="grid grid-cols-2 gap-6">
         {sortedJobs.map((job) => (
           <div 
