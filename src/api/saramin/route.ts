1485b5ad8d51c94f4b52324f7e60b08c2f597281
= <younseu2@gmail.com>
2024-09-10 01:36:59 +0900

    test: swagger api 테스트

--- src/api/saramin/route.ts
+++ src/api/saramin/route.ts
@@ -1,31 +1,19 @@
 import axios from 'axios';
-import { Job } from '@/components/recruit/job';
+import { Job, JobResponse } from '@/components/recruit/job';
 
-const baseUrl = 'https://00f935c6-42a5-448a-8871-ff95c8a2f12a.mock.pstmn.io';
+const baseUrl = 'http://3.39.78.0:8080/api/tech';
 
-export async function fetchJobs(): Promise<Job[]> {
+export async function fetchJobs(params: {
+  search?: string;
+  techName?: string;
+  experienceMin?: number;
+  experienceMax?: number;
+  page?: number;
+  size?: number;
+}): Promise<Job[]> {
   try {
-    const response = await axios.get(`${baseUrl}/job-search`);
-    return response.data.jobs.job.map((job: any) => {
-      return {
-        id: job.id,
-        company: job.company.detail.name,
-        title: job.position.title,
-        industry: job.position.industry,
-        location: job.position.location,
-        jobType: job.position['job-type'],
-        jobMidCode: job.position['job-mid-code'],
-        jobCode: job.position['job-code'],
-        experienceLevel: job.position['experience-level'],
-        requiredEducationLevel: job.position['required-education-level'],
-        postedDate: job['posting-timestamp'] ? new Date(parseInt(job['posting-timestamp']) * 1000).toISOString() : null,
-        timestamp: job['posting-timestamp'] ? parseInt(job['posting-timestamp']) : null,
-        recommendations: job.recommendations || 0,
-        views: job.views || 0,
-        companyImage: job.companyImage || '',
-        url: job.url,
-      };
-    });
+    const response = await axios.get<JobResponse>(`${baseUrl}/job-search`, { params });
+    return response.data.data.content;
   } catch (error) {
     console.error('Error fetching jobs:', error);
     return [];
