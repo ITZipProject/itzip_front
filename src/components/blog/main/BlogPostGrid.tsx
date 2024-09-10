77c3f79c7df724c30d431775bac6be22e9e3ed1b
jukrap <jukrap628@gmail.com>
2024-09-10 19:18:17 +0900

    feat: 카드 컴포넌트 부분 수정 #47

--- src/components/blog/main/BlogPostGrid.tsx
+++ src/components/blog/main/BlogPostGrid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { getAllSubcategories } from '@/data/BlogCategories';
+import { blogCategories, getAllSubcategories } from '@/data/BlogCategories';
 
 import BlogPostCard from './BlogPostCard';
 
@@ -9,18 +9,26 @@ const generateRandomPosts = (count: number) => {
   const authors = ['짱구는멋쟁이', '철수는못말려', '유리는예쁘다', '맹구는똑똑해', '훈이는멋져'];
   const timeAgo = ['1시간 전', '3시간 전', '6시간 전', '12시간 전', '1일 전'];
 
-  return Array.from({ length: count }, (_, i) => ({
-    id: i + 1,
-    title: `블로그 포스트 제목 ${i + 1}`,
-    content: `서류 합격률을 높인 단 한가지 비법 : 멘티 인터뷰 입니다. 서류 합격률을 높은 단 한가지 비법 : 멘티 인터뷰 입니다.`,
-    category: subcategories[Math.floor(Math.random() * subcategories.length)],
-    likes: Math.floor(Math.random() * 100),
-    saves: Math.floor(Math.random() * 50),
-    author: authors[Math.floor(Math.random() * authors.length)],
-    timeAgo: timeAgo[Math.floor(Math.random() * timeAgo.length)],
-    imageUrl: `https://picsum.photos/seed/${i + 1}/600/400`,
-    profileImageUrl: `https://picsum.photos/seed/${i + 1}-profile/40/40`,
-  }));
+  return Array.from({ length: count }, (_, i) => {
+    const subCategory = subcategories[Math.floor(Math.random() * subcategories.length)];
+    const category =
+      Object.keys(blogCategories).find((key) => blogCategories[key].includes(subCategory)) ||
+      '기타';
+
+    return {
+      id: i + 1,
+      title: `블로그 포스트 제목 ${i + 1}`,
+      content: `서류 합격률을 높인 단 한가지 비법 : 멘티 인터뷰 입니다. 서류 합격률을 높은 단 한가지 비법 : 멘티 인터뷰 입니다.`,
+      category,
+      subCategory,
+      likes: Math.floor(Math.random() * 100),
+      saves: Math.floor(Math.random() * 50),
+      author: authors[Math.floor(Math.random() * authors.length)],
+      timeAgo: timeAgo[Math.floor(Math.random() * timeAgo.length)],
+      imageUrl: `https://picsum.photos/seed/${i + 1}/600/400`,
+      profileImageUrl: `https://picsum.photos/seed/${i + 1}-profile/40/40`,
+    };
+  });
 };
 
 const BlogPostGrid: React.FC = () => {
