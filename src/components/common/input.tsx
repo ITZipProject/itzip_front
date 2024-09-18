41eb9d1418db1d0d6f18b6bfe439a42059013865
moko <ljun925@naver.com>
2024-09-18 19:52:56 +0900

    #57-fix-reset 버튼 hover 조정

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -23,14 +23,11 @@ export default function Input({
           name={name}
           {...rest}
         />
-        <button onClick={onClick}>
-          <Image
-            src={resetBtn}
-            width={19}
-            height={19}
-            alt="resetbtn"
-            className="absolute right-[4px] top-5"
-          />
+        <button
+          className="absolute right-[4px] top-3 opacity-0 hover:opacity-100 transition-opacity duration-300 rounded-full p-1 hover:bg-Grey-10"
+          onClick={onClick}
+        >
+          <Image src={resetBtn} width={19} height={19} alt="resetbtn" />
         </button>
       </div>
       <Margin height={'9px'} />
