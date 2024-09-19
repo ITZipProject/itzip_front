718459a2137084ebe12f3f1f57125631e0034771
moko <ljun925@naver.com>
2024-09-19 23:07:15 +0900

    #57-refactor- 타입 분리

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -3,11 +3,7 @@ import { InputHTMLAttributes } from 'react';
 import resetBtn from '../../../public/icons/common/Ic/vector.png';
 import Image from 'next/image';
 import { Margin } from './margin';
-interface InputProps {
-  name: string;
-  errors?: string;
-  onClick?: () => void;
-}
+import { InputProps } from '@/types/input';
 
 export default function Input({
   name,
