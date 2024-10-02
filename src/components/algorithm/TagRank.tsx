d98553ef4b2cc0b382921eef6666bdf5e4096f58
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-02 15:51:11 +0900

    refactor: Tag api 관련 수정

--- src/components/algorithm/TagRank.tsx
+++ src/components/algorithm/TagRank.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 interface Tag {
-  id: number;
-  name: string;
+  bojTagId: number;
+  displayName: string;
 }
 
 interface TagRankProps {
@@ -11,25 +11,75 @@ interface TagRankProps {
 
 const TagRank: React.FC<TagRankProps> = ({ onTagClick }) => {
   const tags: Tag[] = [
-    { id: 1, name: '다이나믹 프로그래밍' },
-    { id: 2, name: '그래프' },
-    { id: 3, name: '그리디' },
-    { id: 4, name: '정렬' },
-    { id: 5, name: '트리' },
-    { id: 6, name: '비트마스크' },
-    { id: 7, name: '수학' },
-    { id: 8, name: '기타' },
-    { id: 9, name: '수학' },
-    { id: 10, name: '기타' },
+    {
+      bojTagId: 25,
+      displayName: '다이나믹 프로그래밍',
+    },
+    {
+      bojTagId: 175,
+      displayName: '자료 구조',
+    },
+    {
+      bojTagId: 33,
+      displayName: '그리디 알고리즘',
+    },
+    {
+      bojTagId: 11,
+      displayName: '그래프 탐색',
+    },
+    {
+      bojTagId: 97,
+      displayName: '정렬',
+    },
+    {
+      bojTagId: 120,
+      displayName: '트리',
+    },
+    {
+      bojTagId: 12,
+      displayName: '이분 탐색',
+    },
+    {
+      bojTagId: 141,
+      displayName: '시뮬레이션',
+    },
+    {
+      bojTagId: 126,
+      displayName: '너비 우선 탐색',
+    },
+    {
+      bojTagId: 127,
+      displayName: '깊이 우선 탐색',
+    },
+    {
+      bojTagId: 5,
+      displayName: '백트래킹',
+    },
+    {
+      bojTagId: 59,
+      displayName: '우선순위 큐',
+    },
+    {
+      bojTagId: 71,
+      displayName: '스택',
+    },
+    {
+      bojTagId: 72,
+      displayName: '큐',
+    },
   ];
 
   return (
     <div className="flex flex-col gap-3 p-4 border border-zinc-600 rounded-xl h-full text-white bg-neutral-800 ">
       <h3 className="text-lg font-semibold mb-2">태그 추천</h3>
-      <div className="w-full h-72 gap-2 border-gray-300 rounded-md shadow-md flex flex-col justify-between items-start ">
+      <div className="w-full gap-2 border-gray-300 rounded-md shadow-md flex flex-col justify-between items-start ">
         {tags.map((tag, index) => (
-          <h3 key={index} onClick={() => onTagClick(tag.id)} className="text-gray-300  text-sm">
-            {tag.id}. {tag.name}
+          <h3
+            key={index}
+            onClick={() => onTagClick(tag.bojTagId)}
+            className="text-gray-300  text-sm"
+          >
+            {index + 1}. {tag.displayName}
           </h3>
         ))}
       </div>
